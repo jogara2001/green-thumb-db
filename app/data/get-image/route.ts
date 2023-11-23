9c6b42a9e2e5c54add10a2bee88f3c89e11435e1
jogara2001 <jogara@calpoly.edu>
2023-11-22 23:17:02 -0800

    minor cleanup for get image

--- app/data/get-image/route.ts
+++ app/data/get-image/route.ts
@@ -6,12 +6,12 @@ export async function POST (request: Request) {
   const url = `https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=1&prop=pageimages&gsrsearch='${search}'`
   const response = await fetch(url)
   const data = await response.json()
-  const firstPage = Object.values(data.query.pages)[0]
+  const firstPage = Object.values(data.query.pages)[0] as any
 
-  const test = `https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&action=query&titles=Image:${firstPage.pageimage}&prop=imageinfo&iiprop=url`
-  const response2 = await fetch(test)
+  const url2 = `https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&action=query&titles=Image:${firstPage.pageimage}&prop=imageinfo&iiprop=url`
+  const response2 = await fetch(url2)
   const data2 = await response2.json()
-  const imageURL = data2.query.pages['-1'].imageinfo[0].url
+  const imageURL = data2.query.pages['-1'].imageinfo[0].url as string
 
   return NextResponse.json({ url: imageURL })
 }
