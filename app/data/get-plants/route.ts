1ccf0650fe6d9b92e620639013fabb6bed7c3f16
Joe O'Gara <jogara@calpoly.edu>
2024-01-02 18:45:41 -0800

    Finished plant pagination
    Finished plant pagination

--- app/data/get-plants/route.ts
+++ app/data/get-plants/route.ts
@@ -4,19 +4,39 @@ import { NextResponse } from 'next/server'
 
 export const dynamic = 'force-dynamic'
 
+const RESULTS_PER_PAGE = 12
+
 export async function POST (request: Request) {
   const supabase = createRouteHandlerClient({ cookies })
-  const { nameFilter, start, end } = await request.json()
+  const { nameFilter, page } = await request.json()
+  const start = page * RESULTS_PER_PAGE
+
+  let { count } = await supabase
+    .from('plants')
+    .select('*', { count: 'exact', head: true })
+    .ilike('common_name', `%${nameFilter}%`)
+
+  if (count == null) {
+    count = 0
+  }
+
+  const totalPages = Math.ceil(count / RESULTS_PER_PAGE)
 
   const { data, error } = await supabase
     .from('plants')
     .select('*')
     .ilike('common_name', `%${nameFilter}%`)
-    .range(start, end)
+    .range(start, start + RESULTS_PER_PAGE - 1)
 
   if (error) {
     console.log(error)
-    return NextResponse.json([])
+    return NextResponse.json({
+      pages: 0,
+      plants: []
+    })
   }
-  return NextResponse.json(data)
+  return NextResponse.json({
+    pages: totalPages,
+    plants: data
+  })
 }
